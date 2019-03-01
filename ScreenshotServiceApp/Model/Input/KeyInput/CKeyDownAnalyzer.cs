2d91123fb2d702d5830f0cab6d559043f661601b
Veronika Domova <veronika.domova@se.abb.com>
2019-03-01 16:57:52 +0100

    Refactor: remove unused directives

--- ScreenshotServiceApp/Model/Input/KeyInput/CKeyDownAnalyzer.cs
+++ ScreenshotServiceApp/Model/Input/KeyInput/CKeyDownAnalyzer.cs
@@ -1,10 +1,4 @@
 ﻿using ScreenshotServiceApp.Controller;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace ScreenshotServiceApp.Model.Input.KeyInput
